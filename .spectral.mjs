c266dc76fedcc3e91946939a5f152bc6fe77f9ea
dddds <neelam.chavan@smartbear.com>
2026-02-26 11:44:47 +0530

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20615.stoplight-dev.com/api/v1/projects/cHJqOjQ2NQ/spectral.js?branch_slug=main&token=e3f2e58d-b299-4887-bba0-c4c079a77390";
+export default { extends: ruleset };
